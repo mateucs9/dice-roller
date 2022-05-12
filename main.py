f61e50fe0fe594d1820a1f963e679d4bc60e8bec
mateucs9 <104104335+mateucs9@users.noreply.github.com>
2022-05-12 09:48:00 +0200

    add dice selection

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import itertools
+from tkinter import ttk
 import tkinter as tk
 from glob import glob
 from PIL import ImageTk, Image
 from random import random
-from time import sleep
 
 class DiceRoller (tk.Tk):
-	def __init__(self, dices):
+	def __init__(self):
 		tk.Tk.__init__(self)
-		self.dices = dices
+		self.dices = 6
+		self.columns = 3
 		self.geometry('600x600')
 		self.title('Dice roller')
 		self.colors = {'dark-brown':  "#cb997e", 
@@ -20,7 +20,7 @@ class DiceRoller (tk.Tk):
 		self.images = glob('images\\*.png')
 		self.configure(background = 'white')
 		self.resizable(0,0)
-		self.dice_size = int(250 / self.dices * (self.dices/2))
+		self.dice_size = int(175 / self.dices * (self.dices/2))
 		self.logos = [ImageTk.PhotoImage(Image.open(self.images[i]).resize((self.dice_size,self.dice_size),Image.ANTIALIAS)) for i in range(len(self.images))]
 		self.result = 0
 		self.widgets = self.get_widgets()
@@ -29,13 +29,38 @@ class DiceRoller (tk.Tk):
 		tk.Label(self, background=self.colors['light-green'], text='Welcome to the Dice Rolling App!', font=('Calibri', 30, 'bold')).pack(pady=30)
 		self.dice_frame = tk.Frame(self, background = 'white')
 		self.dice_frame.pack()
-		for i in range(self.dices):
-			tk.Label(self.dice_frame, image = self.logos[5], borderwidth=0).grid(row=int(i/4), column = i-int(i/4)*4, padx=10)
+		
+		if self.dices < 3:
+			self.columns = self.dices
+		elif self.dices % 3 == 0:
+			self.columns = 3
+		else:
+			self.columns = 4
 
-		tk.Button(self, text='Roll the dice(s)', bg=self.colors['light-green'], font=('Calibri', 15, 'bold'), command=lambda: self.roll_dice()).pack(pady=40)
+		for i in range(self.dices):
+			tk.Label(self.dice_frame, image = self.logos[5], borderwidth=0).grid(row=int(i/self.columns), column = i-int(i/self.columns)*self.columns, padx=10)
+
+		tk.Label(self, text='Select number of dices:', font=('Calibri', 15)).pack(pady=(40,10))
+		self.dice_var = tk.StringVar(self)
+		self.dice_var.trace('w', lambda name, index, mode: self.get_dices_num())
+		
+		self.dice_selection = ttk.Combobox(self, values=list(range(1,13)), state='readonly', textvariable=self.dice_var)
+		self.dice_selection.current(self.dices-1)
+		self.dice_selection.pack()
+		tk.Button(self, text='Roll the dice(s)', bg=self.colors['light-green'], font=('Calibri', 15, 'bold'), command=lambda: self.roll_dice()).pack(pady=20)
 		self.result_lbl = tk.Label(self, text='', font=('Calibri', 15, 'bold'))
 		self.result_lbl.pack()
 	
+	def get_dices_num(self):
+		selection = int(self.dice_selection.get())
+		if selection != self.dices:
+			for widget in self.winfo_children():
+				widget.destroy()
+			
+			self.dices = selection
+			self.get_widgets()
+
+
 	def roll_dice(self):
 		self.result = 0
 		for label in self.dice_frame.winfo_children():
@@ -52,5 +77,5 @@ class DiceRoller (tk.Tk):
 
 
 
-root = DiceRoller(dices=8)
+root = DiceRoller()
 root.mainloop()
